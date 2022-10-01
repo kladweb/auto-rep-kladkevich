e686d6194331def6cf6dac547d5f2fe70e7b9bf1
Pavel Kladkevich <kladpav@gmail.com>
2022-10-01 12:03:07 +0300

    add burger menu and adapt video

--- js/shared.js
+++ js/shared.js
@@ -3,7 +3,9 @@ var modal = document.querySelector(".modal");
 var modalNoButtons = document.querySelectorAll(".modal button");
 var selectContactButton = document.querySelector(".main-nav__button");
 
-selectContactButton.addEventListener("click", function() {
+// modal Window
+
+selectContactButton.addEventListener("click", function () {
   document.body.classList.add('_lock');
   modal.classList.add("open");
   backdrop.classList.add("open");
@@ -21,4 +23,14 @@ function closeModal() {
   }
   backdrop.classList.remove("open");
   document.body.classList.remove('_lock');
+}
+
+// menu_icon (burger)
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.main-nav__items');
+if (iconMenu) {
+  iconMenu.addEventListener('click', function (e) {
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('open');
+  })
 }
